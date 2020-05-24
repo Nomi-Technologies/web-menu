506042eeb8c82c2350830f1a1d01871f3386a7dc
Zhehao /Tony/ Lu <tony1023lv@gmail.com>
2020-05-23 21:56:41 -0700

    Added prod stuff
    Corrected public_url
    
    Remove unnecessary files

---
+++ build/precache-manifest.79d029a246786724548c35d9f5551975.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "239cfb851e0eabb0ae61dd4968534aed",
+    "url": "/web-menu/build/index.html"
+  },
+  {
+    "revision": "2b84ab3212066ae76630",
+    "url": "/web-menu/build/static/css/main.e7d9fdf4.chunk.css"
+  },
+  {
+    "revision": "ac53d23bd1cdca577f18",
+    "url": "/web-menu/build/static/js/2.856097fa.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/web-menu/build/static/js/2.856097fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b84ab3212066ae76630",
+    "url": "/web-menu/build/static/js/main.4c4c4d1c.chunk.js"
+  },
+  {
+    "revision": "c9da2017e7a749055aca",
+    "url": "/web-menu/build/static/js/runtime-main.44fc09f3.js"
+  }
+]);
