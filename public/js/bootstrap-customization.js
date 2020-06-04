f21cfa5d3cec29488a8206e65b2c9844150f811b
Zhehao /Tony/ Lu <tony1023lv@gmail.com>
2020-06-04 00:39:20 -0700

    Removed hack on #root

--- public/js/bootstrap-customization.js
+++ public/js/bootstrap-customization.js
@@ -1,10 +1,3 @@
-function resizeMainContent() {
-  let height = window.innerHeight;
-  document.getElementById('root').style.height = `${height}px`;
-}
-window.addEventListener('resize', resizeMainContent);
-document.addEventListener('DOMContentLoaded', resizeMainContent);
-
 // To center the popup modal in mobile browsers (with tool bars)
 const ob = new MutationObserver(function() {
   const modals = document.body.getElementsByClassName('react-bootstrap-modal');
--- src/App.js
+++ src/App.js
@@ -3,8 +3,29 @@ import './index.css';
 import RestaurantScreen from './screens/RestaurantScreen';
 
 function App() {
+
+  const [windowSize, setWindowSize] = React.useState({ 
+    height: window.innerHeight,
+    width: window.innerWidth
+  })
+
+  React.useEffect(() => {
+    const handleResize = () => {
+      setWindowSize({
+        height: window.innerHeight,
+        width: window.innerWidth
+      });
+    }
+    window.addEventListener('resize', handleResize);
+  });
+  
   return (
-    <div className="App">
+    <div className="App"
+      style={{
+        height: `${windowSize.height}px`,
+        width: `${windowSize.width}px`
+      }}
+    >
       <RestaurantScreen restaurantId={1}/>
     </div>
   );
