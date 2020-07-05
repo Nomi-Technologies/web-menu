24c185dba7359bf1bac75c2e27dd6ffa9f3c2293
Konrad Cheng <konradscheng@gmail.com>
2020-07-04 23:38:08 -1000

    fixed css styles

--- src/wide-screen/components/MenuList.js
+++ src/wide-screen/components/MenuList.js
@@ -5,7 +5,7 @@ import './MenuList.css';
 export default function MenuList(props) {
   return (<div>
     <div className='category-title'>{props.category}</div>
-    <div className='dish-list-wrapper'>
+    <div className='dish-list-wrapper-web'>
       <div className='col-md-6'>
       {props.dishes.slice(0,Math.floor(props.dishes.length / 2)).map(dish =>
         <DishTile key={dish.name} dish={dish}/>
