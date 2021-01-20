bc3f005f2586207582c14c4bba32ced85f6c3586
Zhehao /Tony/ Lu <tony1023lv@gmail.com>
2021-01-20 16:39:16 +0800

    fix(netlify.toml + serviceWorker): added testing env

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -21,7 +21,10 @@ const isLocalhost = Boolean(
 );
 
 export function register(config) {
-  if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+  if ((process.env.NODE_ENV === 'production'
+      || process.env.NODE_ENV === 'staging'
+      || process.env.NODE_ENV === 'testing'
+      ) && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
     if (publicUrl.origin !== window.location.origin) {
