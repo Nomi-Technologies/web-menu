d10d576c4b97d36d0bea37d0e5a82c024b86fff7
Zhehao /Tony/ Lu <tony1023lv@gmail.com>
2021-01-23 23:10:15 +0800

    fix(util.js): replaced outdated g analytics id

--- src/utils.js
+++ src/utils.js
@@ -89,7 +89,7 @@ export const googleAnalyticsPageView = (restaurant) => {
 
   if (process.env.NODE_ENV === 'production' && process.env.REACT_APP_GOOGLE_ANALYTICS_ID) {
     if (window && typeof window !== undefined) {
-      window.gtag('config', 'G-1V27CCNXDJ', { 'page_title': document.title, 'page_path': window.location.pathname })
+      window.gtag('config', process.env.REACT_APP_GOOGLE_ANALYTICS_ID, { 'page_title': document.title, 'page_path': window.location.pathname })
       if(restaurant) {
         window.gtag('event', 'load_restaurant', { restaurant_name: restaurant })
       }
