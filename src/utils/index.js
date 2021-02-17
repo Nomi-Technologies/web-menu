b02f5b6a32acc7e152046678e98dcff1991f8541
Zhehao /Tony/ Lu <tony1023lv@gmail.com>
2021-02-17 19:16:06 +0800

    split into two files

--- src/utils/index.js
+++ src/utils/index.js
@@ -106,13 +106,4 @@ export const getDishImage = async (dishId) => {
   }
 }
 
-export class FilterSet {
-  constructor() {
-    this.allergens = new Set();
-    this.diets = new Set();
-  }
-
-  get size() {
-    return this.allergens.size + this.diets.size;
-  }
-}
+export * from './filter-set';
