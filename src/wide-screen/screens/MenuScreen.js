ec606f60e16a2ea52783c9397b4929b0e7419581
Zach Sullens <zcsullens@zcsullens.local>
2022-11-24 17:23:50 -0800

    replaced all nomi logos with new version

--- src/wide-screen/screens/MenuScreen.js
+++ src/wide-screen/screens/MenuScreen.js
@@ -195,8 +195,7 @@ const NomiBottomLogoImage = styled.a`
     position: relative;
     bottom: 4px;
     display: inline-block;
-    filter: invert(86%) sepia(55%) saturate(2144%) hue-rotate(177deg)
-      brightness(78%) contrast(78%);
+    filter: opacity(50%) contrast(0%);
   }
 `;
 
